0880fc08e117f6d87c34628ec0faa17b756f273e
kel <kelabrantes.me@gmail.com>
2024-12-11 21:04:35 -0300

    primeiro commit

---
+++ pages/product.tsx
@@ -0,0 +1,22 @@
+import { NextPage } from "next";
+import Head from "next/head"
+
+const Products : NextPage = () => {
+    return(
+        <>
+            <Head>
+                <title>
+                    Nossos Produtos
+                </title>
+            </Head>
+
+            <h1>
+                Carrinho
+            </h1>
+        
+        </>
+    )
+
+}
+
+export default Products
